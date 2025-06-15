f9e35541f5f1b33ec5fcffe5399842e4ec7ca254
Nguyễn Cao Minh Tuấn <minhtuantn2406@gmail.com>
2025-06-15 21:10:21 +0700

    refactor: requirements.txt, file structure, credentials file path, readme

--- rsa_key_generator.py
+++ rsa_key_generator.py
@@ -7,7 +7,7 @@ private_key = rsa.generate_private_key(
 )
 
 # Save private key
-with open("private_key.pem", "wb") as f:
+with open("./server/private_key.pem", "wb") as f:
     f.write(private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,
@@ -15,7 +15,7 @@ with open("private_key.pem", "wb") as f:
     ))
 
 # Save public key
-with open("public_key.pem", "wb") as f:
+with open("./client/public_key.pem", "wb") as f:
     f.write(private_key.public_key().public_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
